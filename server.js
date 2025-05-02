f32eff5f3fd56ad9bdfcef83843f511efdc17c30
rishaviitd <ms1210873@iitd.ac.in>
2025-05-03 01:52:42 +0530

    added some local developmen routes

--- server.js
+++ server.js
@@ -30,6 +30,8 @@ const corsOptions = {
     "http://localhost:3000",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "http://localhost:8080",
+    "http://localhost:8081",
   ],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization", "x-auth-token"],
